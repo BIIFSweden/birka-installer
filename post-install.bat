2026ba7f7d0b1758a230dcc2dcb3a0ade01ca78d
Jonas Windhager <jonas.windhager@scilifelab.se>
2024-02-11 23:00:51 +0100

    minor changes

--- post-install.bat
+++ post-install.bat
@@ -1,2 +1,2 @@
 call "%PREFIX%\Scripts\activate.bat"
-python -m pip install aicspylibczi fsspec readlif pyside6-essentials
+python -m pip install aicspylibczi>=3.1.1 fsspec>=2022.8.0 readlif>=0.6.4
--- post-install.sh
+++ post-install.sh
@@ -1,2 +1,2 @@
 source "$PREFIX/etc/profile.d/conda.sh" && conda activate "$PREFIX"
-python -m pip install aicspylibczi fsspec readlif pyside6-essentials
+python -m pip install aicspylibczi>=3.1.1 fsspec>=2022.8.0 readlif>=0.6.4
